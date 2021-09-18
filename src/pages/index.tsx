b290fdf6b866284aa5f99bdfd80a45a39301ce2f
portaldev <portaldev21@gmail.com>
2021-09-18 14:30:57 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-8xl">Hello Word</h1>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'ui-sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
+
